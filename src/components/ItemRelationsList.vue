3f086d447579b393243f2a204c9adcbf16a38778
Slava Pedak <team@openpim.org>
2020-10-22 15:46:41 +0300

    fix for ability to set page size for relations

--- src/components/ItemRelationsList.vue
+++ src/components/ItemRelationsList.vue
@@ -65,13 +65,13 @@
               </tbody>
             </template>
           </v-simple-table>
-          <v-container class="pa-0">
+          <v-container class="pa-0" v-if="componentType === 'source' ? sourceRelationsTotal[identifier] > pageSize : targetRelationsTotal[identifier] > pageSize">
             <v-row>
               <v-col cols="2">
                 <v-text-field type="number" v-model="pageSize" :label="$t('ItemRelationsList.RowsPerPage')" required :rules="[required, pageSizePositive]" @change="pageSizeChanged(identifier)"></v-text-field>
               </v-col>
               <v-col cols="2" class="mt-5">
-                <span>{{ $t('ItemRelationsList.TotalRows') + ' ' + targetRelationsTotal[identifier]}}</span>
+                <span>{{ $t('ItemRelationsList.TotalRows') + ' ' + (componentType === 'source' ? sourceRelationsTotal[identifier] : targetRelationsTotal[identifier])}}</span>
               </v-col>
               <v-col cols="8">
                 <template v-if="pageSize && pageSize !== '0'">
