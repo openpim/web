63b3ff9151916ace31e140f3ed4d0753b8bb3460
openpim <team@openpim.org>
2021-05-26 15:18:47 +0300

    channels (in progress)

--- src/channels/ext/ExtConfigCompoment.vue
+++ src/channels/ext/ExtConfigCompoment.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>EXT !!!</div>
+  <div>
+    <v-text-field v-if="channel" v-model="channel.config[channel.identifier].extCmd"  :label="$t('Channels.Ext.cmd')" required></v-text-field>
+  </div>
 </template>
 <script>
+import { watch } from '@vue/composition-api'
 
 export default {
-  setup () {
+  props: {
+    channel: {
+      required: true
+    }
+  },
+  setup (props, { root }) {
+    watch(() => props.channel, (chan, previousValue) => {
+      if (chan && !chan.config[chan.identifier]) {
+        root.$set(chan.config, chan.identifier, { extCmd: '' })
+      }
+    })
+
     return {
     }
   }
