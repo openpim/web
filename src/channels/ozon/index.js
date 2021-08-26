81fc23c1e78d92e130ee6fdd6b82b5f9a12fffd9
openpim <team@openpim.org>
2021-08-26 17:14:26 +0300

    ozon (in progress)

--- src/channels/ozon/index.js
+++ src/channels/ozon/index.js
@@ -2,8 +2,50 @@ function getConfigCompoment () { return 'OzonConfigCompoment' }
 function getStandardAttributes () {
   return [
     {
+      id: '#productCode',
+      name: 'Артикул товара',
+      required: true,
+      dictionary: false
+    },
+    {
+      id: '#name',
+      name: 'Наименование',
+      required: false,
+      dictionary: false
+    },
+    {
       id: '#barcode',
       name: 'Баркод',
+      required: true,
+      dictionary: false
+    },
+    {
+      id: '#price',
+      name: 'Цена',
+      required: true,
+      dictionary: false
+    },
+    {
+      id: '#weight',
+      name: 'Вес с упаковкой, г',
+      required: true,
+      dictionary: false
+    },
+    {
+      id: '#depth',
+      name: 'Длина упаковки, мм',
+      required: true,
+      dictionary: false
+    },
+    {
+      id: '#width',
+      name: 'Ширина упаковки, мм',
+      required: true,
+      dictionary: false
+    },
+    {
+      id: '#height',
+      name: 'Высота упаковки, мм',
       required: true,
       dictionary: false
     }
