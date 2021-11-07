afae4477238fe92adcbf76c89b420548d46f757c
openpim <team@openpim.org>
2021-11-07 17:17:00 +0300

    Ozon improvements

--- src/channels/ozon/OzonConfigCompoment.vue
+++ src/channels/ozon/OzonConfigCompoment.vue
@@ -2,6 +2,8 @@
   <div>
     <v-text-field v-if="channel" v-model="channel.config.ozonClientId" :readonly="readonly" label="Client ID" required></v-text-field>
     <v-text-field v-if="channel" v-model="channel.config.ozonApiKey" :readonly="readonly" label="Api-Key" required></v-text-field>
+    <v-text-field v-if="channel" v-model="channel.config.ozonIdAttr" :readonly="readonly" label="Атрибут где хранить ID" required></v-text-field>
+    <v-text-field v-if="channel" v-model="channel.config.ozonImageAttr" :readonly="readonly" label="Атрибут где лежит URL изображения" required></v-text-field>
     <MappingConfigCompoment v-if="channel" :channel="channel" :readonly=readonly :variants="false"></MappingConfigCompoment>
 
     <template v-if="channel">
