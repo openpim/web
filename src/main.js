26bcded4ea8ca129f219599a7a59c09c917351e3
chcat2014 <ch_cat@mail.ru>
2022-12-09 17:59:17 +0300

    fixed login

--- src/main.js
+++ src/main.js
@@ -4,12 +4,19 @@ import App from './App.vue'
 import router from './router'
 
 import vuetify from './plugins/vuetify'
-import i18n from './i18n'
+import i18n, { loadLocaleMessages } from './i18n'
+
+async function init () {
+  await loadLocaleMessages(i18n, process.env.VUE_APP_I18N_LOCALE || 'en')
+
+  vue.mount('#app')
+}
 
 const vue = createApp(App)
 vue.use(router)
 vue.use(vuetify)
 vue.use(i18n)
-vue.mount('#app')
+
+init()
 
 export default vue
