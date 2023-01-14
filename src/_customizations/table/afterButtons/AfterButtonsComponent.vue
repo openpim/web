27c7080fa94a3d9298336900c0e2c9c9083bf3e9
openpim <team@openpim.org>
2023-01-14 15:56:47 +0300

    add more data to custom component

--- src/_customizations/table/afterButtons/AfterButtonsComponent.vue
+++ src/_customizations/table/afterButtons/AfterButtonsComponent.vue
@@ -6,7 +6,16 @@ export default {
   props: {
     headers: {
       required: true
-    }
+    },
+    columnsSelected: {
+      required: true
+    },
+    items: {
+      required: true
+    },
+    loadData: {
+      required: true
+    },
   },
   setup (props) {
     return {
