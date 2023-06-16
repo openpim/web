da6fbd58c46393bd72a96e969c41628256595fd9
Daniil Navolockii <daniilnavolockii@mail.ru>
2023-06-16 16:12:15 +0300

    Dashboard shows zero and fix regex null value

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <BarChart v-if="component.chart === 1 && loadedRef" :data="dataRef" :options="{onClick: function(event, item){chartClick(event, item)},legend: {display: false}, title: {display: true, text: component.title}}"></BarChart>
+    <BarChart v-if="component.chart === 1 && loadedRef" :data="dataRef" :options="{onClick: function(event, item){chartClick(event, item)},legend: {display: false}, title: {display: true, text: component.title},scales: {yAxes: [{ticks: {beginAtZero: true}}]}}"></BarChart>
     <HorizontalBarChart v-if="component.chart === 2 && loadedRef" :data="dataRef" :options="{onClick: function(event, item){chartClick(event, item)},legend: {display: false}, title: {display: true, text: component.title}}"></HorizontalBarChart>
     <CircleChart v-if="component.chart === 3 && loadedRef" :data="dataRef" :options="{onClick: function(event, item){chartClick(event, item)},title: {display: true, text: component.title}}"></CircleChart>
     <PieChart v-if="component.chart === 4 && loadedRef" :data="dataRef" :options="{onClick: function(event, item){chartClick(event, item)},title: {display: true, text: component.title}}"></PieChart>
