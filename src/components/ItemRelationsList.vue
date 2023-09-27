0cac208a0cbdd1ec1d7437c7366b6d658af63459
Vyacheslav Pedak <vpedak@gmail.com>
2023-09-27 14:18:16 +0300

    filter by relation

--- src/components/ItemRelationsList.vue
+++ src/components/ItemRelationsList.vue
@@ -32,8 +32,8 @@
 
                   <th class="text-left pa-0" v-for="(attr, j) in getAttributesForRelation(identifier)" :key="'A'+j" :width="getOption2(attr, 'tableWidth', '')">
                     <div>{{attr.name[currentLanguage.identifier] || '[' + attr.name[defaultLanguageIdentifier] + ']'}}</div>
-                    <input v-if="!attr.lov && attr.type !== 2" type="text" style="border: solid; border-color: grey; border-width: 1px" v-model="attr.IRfilter" @input="filterChanged(identifier, attr)"/>
-                    <v-autocomplete v-if="attr.lov && attr.type !== 2" v-model="attr.IRfilter" :items="getLOVItems(attr.lov)" dense clearable class="ml-2 mr-2" @input="filterChanged(identifier, attr)"></v-autocomplete>
+                    <input v-if="!attr.lov && attr.type !== 2" type="text" style="border: solid; border-color: grey; border-width: 1px" v-model="attr['IRfilter'+identifier]" @input="filterChanged(identifier, attr)"/>
+                    <v-autocomplete v-if="attr.lov && attr.type !== 2" v-model="attr['IRfilter'+identifier]" :items="getLOVItems(attr.lov)" dense clearable class="ml-2 mr-2" @input="filterChanged(identifier, attr)"></v-autocomplete>
                     <!-- skip type = 2 (boolean) for now because data can be true, false, empty and also the control should have ability to show not filter
                       all this is impossible to make with usual checkbox -->
                   </th>
@@ -685,14 +685,14 @@ export default {
     let currentFilter = {}
     function filterChanged (relationIdentifier, attr) {
       if (currentFilter[relationIdentifier]) {
-        if (attr.IRfilter) {
-          currentFilter[relationIdentifier][attr.identifier] = attr.IRfilter
+        if (attr['IRfilter' + relationIdentifier]) {
+          currentFilter[relationIdentifier][attr.identifier] = attr['IRfilter' + relationIdentifier]
         } else {
           if (currentFilter[relationIdentifier][attr.identifier]) delete currentFilter[relationIdentifier][attr.identifier]
         }
       } else {
         const tmp = {}
-        tmp[attr.identifier] = attr.IRfilter
+        tmp[attr.identifier] = attr['IRfilter' + relationIdentifier]
         currentFilter[relationIdentifier] = tmp
       }
       pageChanged(relationIdentifier)
