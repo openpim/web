dbb76c1e5ecbc1cb6c0016897fefcfc41d71a56b
Vyacheslav Pedak <vpedak@gmail.com>
2024-06-17 17:04:47 +0300

    add date format

--- src/components/SystemInformation.vue
+++ src/components/SystemInformation.vue
@@ -12,14 +12,15 @@
       <v-list-item><v-list-item-title>{{ 'id: ' + (data.internalId || data.id) }}</v-list-item-title></v-list-item>
       <v-list-item><v-list-item-title>{{ 'identifier: ' + data.identifier }}</v-list-item-title></v-list-item>
       <v-list-item><v-list-item-title>{{ $t('CreatedBy') + ': ' + data.createdBy }}</v-list-item-title></v-list-item>
-      <v-list-item><v-list-item-title>{{ $t('CreatedAt') + ': ' + data.createdAt }}</v-list-item-title></v-list-item>
+      <v-list-item><v-list-item-title>{{ $t('CreatedAt') + ': ' + dateFormat(data.createdAt, DATE_FORMAT) }}</v-list-item-title></v-list-item>
       <v-list-item><v-list-item-title>{{ $t('UpdatedBy') + ': ' + data.updatedBy }}</v-list-item-title></v-list-item>
-      <v-list-item><v-list-item-title>{{ $t('UpdatedAt') + ': ' + data.updatedAt }}</v-list-item-title></v-list-item>
+      <v-list-item><v-list-item-title>{{ $t('UpdatedAt') + ': ' + dateFormat(data.updatedAt, DATE_FORMAT) }}</v-list-item-title></v-list-item>
     </v-list>
   </v-menu>
 </template>
 <script>
 import { ref } from '@vue/composition-api'
+import dateFormat from 'dateformat'
 
 export default {
   props: {
@@ -31,7 +32,9 @@ export default {
     const showMenuRef = ref(false)
 
     return {
-      showMenuRef
+      showMenuRef,
+      dateFormat,
+      DATE_FORMAT: process.env.VUE_APP_DATE_FORMAT
     }
   }
 }
