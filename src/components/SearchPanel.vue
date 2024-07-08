a9db26b6277144d426f8cd29490a04a0c5c49d0d
openpimdev <developer.kaproductions@gmail.com>
2024-07-08 15:53:30 +0300

    fix for search

--- src/components/SearchPanel.vue
+++ src/components/SearchPanel.vue
@@ -1,10 +1,18 @@
 <template>
   <v-row v-if="hasAccess('search') || hasAccess('searchRelations')">
     <v-col cols="12">
-      <v-select v-model="searchEntityRef" :items="items" @change="onSearchEntityChange" dense hide-details class="mx-4 mb-0 py-0" />
-      <SearchItem v-if="searchEntityRef === 'ITEM'" />
-      <SearchItemRelation v-if="searchEntityRef === 'ITEM_RELATION'" />
-      <template v-if="searchEntityRef !== 'ITEM' && searchEntityRef !== 'ITEM_RELATION'">
+      <v-select v-model="searchVal" :items="items" @change="onSearchEntityChange" dense hide-details class="mx-4 mb-0 py-0" />
+      <v-input
+          v-if="selectedRef && selectedRef.identifier && selectedRef.identifier.length"
+          class="mx-4 mb-0 py-0"
+          append-icon="mdi-close"
+          @click:append="clearSelectedRef"
+        >
+        {{ selectedRef.name[currentLanguage.identifier] || selectedRef.name[defaultLanguageIdentifier.identifier] }}
+      </v-input>
+      <SearchItem v-if="searchVal === 'ITEM'" />
+      <SearchItemRelation v-if="searchVal === 'ITEM_RELATION'" />
+      <template v-if="searchVal !== 'ITEM' && searchVal !== 'ITEM_RELATION'">
         <CustomSearchComponent  v-for="(elem, i) in customSearch()" :key="i" searchEntity="searchEntityRef"/>
       </template>
     </v-col>
@@ -20,13 +28,16 @@ import SearchItem from './SearchItem.vue'
 import SearchItemRelation from './SearchItemRelation.vue'
 import customSearch from '../_customizations/search/customSearch.js'
 import CustomSearchComponent from '../_customizations/search/customSearchComponent.vue'
+import router from '../router'
+import { ref } from '@vue/composition-api'
 
 export default {
   components: { SearchItem, SearchItemRelation, CustomSearchComponent },
   setup (props, context) {
     const { currentWhereRef, searchEntityRef, selectedRef } = searchStore.useStore()
     const { hasAccess } = userStore.useStore()
-    const { currentLanguage } = langStore.useStore()
+    const { currentLanguage, defaultLanguageIdentifier } = langStore.useStore()
+    const searchVal = ref('ITEM')
 
     function onSearchEntityChange (val) {
       localStorage.setItem('last_search_entity', val)
@@ -34,11 +45,25 @@ export default {
       name[currentLanguage.value.identifier] = i18n.t('SearchSaveDialog.NameNew')
       currentWhereRef.value = null
       selectedRef.value = { identifier: '', entity: val, name: name, filters: [], whereClause: {}, extended: false, public: false, orAnd: 1 }
+      router.push('/search')
+      searchEntityRef.value = val
+    }
+
+    function clearSelectedRef () {
+      const name = {}
+      name[currentLanguage.value.identifier] = i18n.t('SearchSaveDialog.NameNew')
+      currentWhereRef.value = null
+      selectedRef.value = { identifier: '', entity: searchEntityRef.value.valuel, name: {}, filters: [], whereClause: {}, extended: false, public: false, orAnd: 1 }
+      router.push('/search')
     }
 
     return {
       hasAccess,
-      searchEntityRef,
+      searchVal,
+      selectedRef,
+      currentLanguage,
+      defaultLanguageIdentifier,
+      clearSelectedRef,
       currentWhereRef,
       onSearchEntityChange,
       customSearch,
