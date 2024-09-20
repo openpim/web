ef37e47de8ac81cf215c73a7a499e3017e07be9b
Daniil Navolockii <daniilnavolockii@mail.ru>
2024-09-20 15:31:33 +0300

    prohibition of password autocomplete

--- src/channels/mdmExt/MDMExtConfigComponent.vue
+++ src/channels/mdmExt/MDMExtConfigComponent.vue
@@ -56,7 +56,7 @@
         </v-row>
         <v-row class="pt-0 mt-0">
           <v-col><v-text-field v-model="masterLogin" @change="masterLoginChanged" :label="$t('MappingConfigComponent.MasterLogin')" dense/></v-col>
-          <v-col><v-text-field v-model="masterPassword" @change="masterPasswordChanged" :label="$t('MappingConfigComponent.MasterPassword')" dense type="password"/></v-col>
+          <v-col><v-text-field v-model="masterPassword" @change="masterPasswordChanged" :label="$t('MappingConfigComponent.MasterPassword')" dense type="password" autocomplete="new-password"/></v-col>
         </v-row>
       </v-card-text>
     </v-card>
