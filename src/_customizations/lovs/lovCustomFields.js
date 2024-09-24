0d2780d417c5889a44c4c86e9a06a7e4e35a3d87
Vyacheslav Pedak <vpedak@gmail.com>
2024-09-24 18:14:32 +0300

    type to lov custom field

--- src/_customizations/lovs/lovCustomFields.js
+++ src/_customizations/lovs/lovCustomFields.js
@@ -1,4 +1,5 @@
 export default function (lovIdentifier) {
   // can return array of object like { identifier: 'data', name: 'Addition data', readonly: true }
+  // can be also a checkbox { identifier: 'data', name: 'Addition data', readonly: false, type: 'bool' }
   return []
 }
