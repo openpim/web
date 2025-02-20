4054a7e55ec4e4f38cb2f0640d4f4d4ddac7deca
Vyacheslav Pedak <vpedak@gmail.com>
2025-02-20 17:15:40 +0300

    title support

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,21 @@
 
 <script>
 import initStores from './store'
+import { onMounted } from '@vue/composition-api'
+import * as userStore from './store/users'
 
 export default {
   setup () {
     initStores()
+
+    const {
+      getServerConfig
+    } = userStore.useStore()
+
+    onMounted(async () => {
+      const serverConfig = await getServerConfig()
+      if (serverConfig.title) document.title = serverConfig.title
+    })
   }
 }
 </script>
